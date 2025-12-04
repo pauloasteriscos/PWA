0d0c67553df6f1ea073dc085b225638f3f324ff7
Paulo Asteriscos <48494465+pauloasteriscos@users.noreply.github.com>
2025-12-04 01:38:02 +0000

    Delete MiniProj2-Back directory

--- MiniProj2-Back/routes/home.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-let router = express.Router();
-const HomeController = require('../controllers/home.controller');
-
-router.route('/')
-    .get(HomeController.get);
-
-module.exports = router;
